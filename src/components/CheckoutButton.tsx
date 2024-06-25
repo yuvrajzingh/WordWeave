9a5c90b8c540e2da787dd6f7af2ce695325139c3
Yuvraj Singh <yuvisingh1810@gmail.com>
2024-06-25 23:43:23 +0530

    CheckoutButton

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -1,26 +1,36 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { useSession } from "next-auth/react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const CheckoutButton = () => {
+  const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
 
-    const { data: session } = useSession();
+  const createCheckoutSession = async () => {
+    if (!session?.user.id) return;
+    // push a doc into firestore db
+    setLoading(true);
 
-    const createCheckoutSession = async ()=>{
-        if(!session) return;
-        // push a doc into firestore db
+    const docRef = await addDoc(
+      collection(db, "customer", session.user.id, "checkout_session"),
+      {
+        price: "", //TODO:set price
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      }
+    );
+    // ... stripe extension on firebase will create a checkout session
 
-        // ... stripe extension on firebase will create a checkout session
-
-        // redirect user to checkout page
-    }
+    // redirect user to checkout page
+  };
 
   return (
     <div className="flex flex-col space-y-2">
-
-        {/* if subscribed show me the user is subscribed  */}
+      {/* if subscribed show me the user is subscribed  */}
 
       <Button className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default">
         Sign Up
