02a2086bf8f37a486c4f3f8545e283dc242fc579
Yuvraj Singh <yuvisingh1810@gmail.com>
2024-07-29 23:57:36 +0530

    building chatList && chatList rows

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,10 +1,22 @@
 import { getServerSession } from 'next-auth'
 import { authOptions } from '../../auth'
+import { getDocs } from 'firebase/firestore'
+import { chatMembersCollectionGroupRef } from '@/lib/converters/ChatMembers';
+import ChatListRows from './ChatListRows';
 
 async function ChatList() {
     const session = await getServerSession(authOptions)
+
+    const chatsSnapshot = await getDocs(
+      chatMembersCollectionGroupRef(session?.user.id!) 
+    )
+
+    const initialChats = chatsSnapshot.docs.map((doc) =>({
+      ...doc.data(),
+      timestamp: null,
+    }))
   return (
-    <div>ChatList</div>
+    <ChatListRows initialChats={initialChats}/>
   )
 }
 
---
+++ src/components/ChatListRow.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+function ChatListRow({chatId} : {chatId: string}) {
+  return (
+    <div>ChatListRow</div>
+  )
+}
+
+export default ChatListRow
