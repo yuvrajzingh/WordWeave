42b985260d4857f08643605f88313a5cb50c3396
Yuvraj Singh <yuvisingh1810@gmail.com>
2024-08-26 23:25:35 +0530

    create language menu

--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import { usePathname } from "next/navigation";
-import { useLanguageStore, useSubscriptionStore } from "../../store/store";
+import { LanguagesSupported, LanguagesSupportedMap, useLanguageStore, useSubscriptionStore } from "../../store/store";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import LoadingSpinner from "./LoadingSpinner";
+import Link from "next/link";
+
+
 
 function LanguageSelect() {
-  const [language, setLanguage, getLanguage, getNoSupportedLanguages ] = 
+  const [language, setLanguage, getLanguages, getNoSupportedLanguages ] = 
   useLanguageStore((state)=>[
     state.language,
     state.setLanguage,
@@ -21,7 +26,44 @@ function LanguageSelect() {
   const isChatPage = pathName.includes("/chat");
 
   return isChatPage && (
-    <div>LanguageSelect</div>
+    <div>
+      <Select
+        onValueChange={(value: LanguagesSupported) => setLanguage(value)}
+      >
+        <SelectTrigger className="w-[150px] text-black dark:text-white">
+          <SelectValue
+            placeholder={LanguagesSupportedMap[language]}
+            className=""
+          />
+        </SelectTrigger>
+
+        <SelectContent>
+          {subscription === undefined ? (
+            <LoadingSpinner />
+          ) : (
+            <>
+              {getLanguages(isPro).map((language)=> (
+                <SelectItem key={language} value={language}>
+                  {LanguagesSupportedMap[language]}
+                </SelectItem>
+              ))}
+              {getNoSupportedLanguages(isPro).map((language)=>(
+                <Link href={"/register"} key={language} prefetch={false}>
+                  <SelectItem
+                    key={language}
+                    value={language}
+                    disabled
+                    className="bg-gray-300/50 text-gray-500 dark:text-white py-2 my-1"
+                  >
+                    {LanguagesSupportedMap[language]} (PRO)
+                  </SelectItem>
+                </Link>
+              ))}
+            </>
+          )}  
+        </SelectContent>        
+      </Select>
+    </div>
   )
 }
 
